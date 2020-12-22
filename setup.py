b4291bfbdaf86e1bce82b5b573a80c04b618bd9d
garlontas <stefan.garlonta@gmail.com>
2020-12-22 12:09:36 +0100

    Created file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FFRipper',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/ThePickwickClub/ffripper',
+    license='GPLv3',
+    author='Stefan Garlonta',
+    author_email='stefan.garlonta@gmail.com',
+    description='A fast audio-cd ripper'
+)
