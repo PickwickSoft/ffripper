8e2d84efe14d9edee70ec8acd790a085c073557c
Stefan Garlonta <70283087+garlontas@users.noreply.github.com>
2021-07-05 16:10:04 +0200

    Add assume yes option to apt

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo apt install python3-pip python3-gi python3-libdiscid python3-distutils
+sudo apt install -y python3-pip python3-gi python3-libdiscid python3-distutils
 pip3 install -r requirements.txt
 sudo python3 setup.py install
